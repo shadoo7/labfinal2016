6eec7f8b18aa1ff298b7c4cb51c81d0dc309595a
Nvidia Geforce <Nvidia Geforce@192.168.10.65>
2016-06-20 14:01:45 +0500

    this is final github repositry

---
+++ src/finalpakg/Bank.java
@@ -0,0 +1,46 @@
+package finalpakg;
+
+/**
+ * 
+ * Extending functionality class
+ * @author Shehzad Raheem 
+ *
+ */
+
+
+public class Bank extends Account {
+	
+	/**
+	 * Adding bank account
+	 * @param a
+	 */
+	
+	
+	
+	
+	public void addAccount(Account a){
+		
+	}
+     public Account selectAccount(int no){
+    	 
+    	 
+    	 Account account = null;
+    	 
+    	 return account;
+     }
+     
+     /**
+      * Override from account class
+      * 
+      */
+     
+     
+    	 
+    @Override 
+    public boolean withdraw(double d){
+    	//sssssss
+    	
+    return false;	 
+    	
+    }
+}
